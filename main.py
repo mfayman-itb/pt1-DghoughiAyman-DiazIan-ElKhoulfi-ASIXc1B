e5031bf8af35058cf8685d2a0e789137565f7e65
mfayman <ayman.dghoughi.7e7@itb.cat>
2024-02-28 18:43:44 +0100

    updt

--- main.py
+++ main.py
@@ -1,62 +1,52 @@
+#region Imports
 import random
 import string
+#endregion
 
+#region Variables i constants
 ALLOWED = string.ascii_letters + string.digits
-blacklist = []
-#region defs
+#endregion
+
+#region Funcions
 def get_input():
-    text = input('putin: ')
-    words = text.split()
+    text = input('Ingrese el texto: ')
+    return text
+
+def fix_punctuation(text):
+    words = []
+    current_word = ''
+    for char in text:
+        if char in ALLOWED:
+            current_word += char
+        else:
+            if current_word:
+                words.append(current_word)
+                current_word = ''
+            words.append(char)
+    if current_word:
+        words.append(current_word)
     return words
 
-def fix_punctuation(words):
-    punct = ""
-    current_chunk = ""
-    pos = ""
-    result = []
-    for word in words:
-        for char in word:
-            if char in ALLOWED:
-                current_chunk += char
-            elif char not in ALLOWED and (word.index(char) == 0 or word[:-1].index(char) == len(word) - 1):
-                if word.index(char) == 0:
-                    pos = 0
-                else:
-                    pos = 1
-                punct = char
-            else:
-                #blacklist.append(char)
-                current_chunk += char
-        if pos == 0:
-            result.append(punct)
-            result.append(current_chunk)
-        else:
-            result.append(current_chunk)
-            result.append(punct)
-    print(result)
-    return result
-
-
-
-def disorder_words(result):
+def disorder_words(words):
     disorder_text = []
-    for word in result:
-        #for punct in blacklist:
-        if len(word) <= 3:
+    for word in words:
+        if len(word) <= 3 or not any(char in ALLOWED for char in word[1:-1]):
             disorder_text.append(word)
-            continue
-        first_letter = word[0]
-        last_letter = word[-1]
-        middle_letters = list(word[1:-1])  # Corregido aquí
-        random.shuffle(middle_letters)
-        disorder_word = first_letter + ''.join(middle_letters) + last_letter
-        disorder_text.append(disorder_word)
-    return ' '.join(disorder_text)
+        else:
+            first_letter = word[0]
+            last_letter = word[-1]
+            middle_letters = list(word[1:-1])
+            random.shuffle(middle_letters)
+            disorder_word = first_letter + ''.join(middle_letters) + last_letter
+            disorder_text.append(disorder_word)
+    return ''.join(disorder_text)
 
 def main():
     text = get_input()
-    result = fix_punctuation(text)
-    print(disorder_words(result))
+    words = fix_punctuation(text)
+    print(disorder_words(words))
 #endregion
 
+#region main
 main()
+#endregion
