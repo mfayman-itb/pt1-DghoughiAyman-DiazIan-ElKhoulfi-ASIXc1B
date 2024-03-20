22c685c0ff5b88066d6a4df698ed66db72aaab55
Ian Díaz Pérez <145555931+iand-itb@users.noreply.github.com>
2024-03-20 13:06:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,39 @@
 #region Imports
+from data_source import *
+from crazy_words import *
 import random
 import string
 #endregion
 
 #region Variables i constants
-ALLOWED = string.ascii_letters + string.digits
 #endregion
 
 #region Funcions
-def get_input():
-    text = input('Ingrese el texto: ')
-    return text
-
-def fix_punctuation(text):
-    words = []
-    current_word = ''
-    for char in text:
-        if char in ALLOWED:
-            current_word += char
-        else:
-            if current_word:
-                words.append(current_word)
-                current_word = ''
-            words.append(char)
-    if current_word:
-        words.append(current_word)
-    return words
-
-def disorder_words(words):
-    disorder_text = []
-    for word in words:
-        if len(word) <= 3 or not any(char in ALLOWED for char in word[1:-1]):
-            disorder_text.append(word)
-        else:
-            first_letter = word[0]
-            last_letter = word[-1]
-            middle_letters = list(word[1:-1])
-            random.shuffle(middle_letters)
-            disorder_word = first_letter + ''.join(middle_letters) + last_letter
-            disorder_text.append(disorder_word)
-    return ''.join(disorder_text)
+def menu():
+    try:
+        options = ['1','2','3','4']
+        print(f"1:\tGet the data from the keyboard.\n2:\tGet the data from an URL.\n3:\tGet the data from the answer to a ChatGPT prompt.\n4:\tGet the data from a file.")
+        inp = input("Select option: ")
+        while str(inp) not in options:
+            print(f'Option "{inp}" not available.')
+            print(f"1:\tGet the data from the keyboard.\n2:\tGet the data from an URL.\n3:\tGet the data from the answer to a ChatGPT prompt.\n4:\tGet the data from a file.")
+            inp = int(input("Select option: "))
+        match inp:
+            case '1': text = get_input(inp)
+            case '2': text = get_data_from_server(inp)
+            case '3':
+                apikey = get_gpt_api_key(inp)
+                text = get_data_from_chatgpt(apikey, inp)
+            case '4': text = get_data_from_file(inp)
+        return text
+    except Exception as e:
+        print(e)
 
 def main():
-    text = get_input()
-    words = fix_punctuation(text)
-    print(disorder_words(words))
+    text = menu()
+    clean = fix_punctuation(text)
+    result = disorder_words(clean)
+    return print(result)
 #endregion
 
 #region main
