b710c84a0b38bb96ccef9be0954cc371f55884b5
Ian Díaz Pérez <145555931+iand-itb@users.noreply.github.com>
2024-03-21 10:30:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,18 +15,18 @@ import string
 def menu():
     try:
         options = ['1','2','3','4']
-        print(f"1:\tGet the data from the keyboard.\n2:\tGet the data from an URL.\n3:\tGet the data from the answer to a ChatGPT prompt.\n4:\tGet the data from a file.")
+        print(f"\n1:\tGet the data from the keyboard.\n2:\tGet the data from an URL.\n3:\tGet the data from the answer to a ChatGPT prompt.\n4:\tGet the data from a file.\n")
         inp = input("Select option: ")
         while str(inp) not in options:
             print(f'Option "{inp}" not available.')
-            print(f"1:\tGet the data from the keyboard.\n2:\tGet the data from an URL.\n3:\tGet the data from the answer to a ChatGPT prompt.\n4:\tGet the data from a file.")
+            print(f"\n1:\tGet the data from the keyboard.\n2:\tGet the data from an URL.\n3:\tGet the data from the answer to a ChatGPT prompt.\n4:\tGet the data from a file.\n")
             inp = int(input("Select option: "))
         match inp:
             case '1': text = get_input(inp)
             case '2': text = get_data_from_server(inp)
             case '3':
                 apikey = get_gpt_api_key()
-                text = get_data_from_chatgpt(apikey, inp)
+                text = get_data_from_chatgpt(inp)
             case '4': text = get_data_from_file(inp)
         return text
     except Exception as e:
